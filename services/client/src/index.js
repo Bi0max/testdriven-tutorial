6e9466f1d44051e1f448a1f53f7679e69525ac08
Maksim Rodin <rodin.maxim93@gmail.com>
2019-02-11 09:26:32 +0100

    Add submission form in React

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: '',
     };
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -21,10 +26,17 @@ class App extends Component {
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">
               <br/>
               <h1 className="title is-1">All Users</h1>
               <hr/><br/>
+              <AddUser 
+                addUser={this.addUser}
+                username={this.state.username}
+                email={this.state.email}
+                handleChange={this.handleChange}
+              />
+              <br/><br/>
               <UsersList users={this.state.users}/>
             </div>
           </div>
@@ -33,9 +45,31 @@ class App extends Component {
     )
   }
 
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
+
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)  // new
     .then((res) => { this.setState({ users: res.data.data.users }); })
+    .catch((err) => { console.log(err); });
+  };
+
+  addUser(event) {
+    event.preventDefault();
+    console.log('sanity check!');
+
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    .then((res) => {
+      this.getUsers();
+      this.setState({ username: '', email: '' });
+    })
     .catch((err) => { console.log(err); });
   };
 };
